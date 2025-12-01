d1ca1ceeeade771820a425e8ab69262527a2f85a
Timur <136839888+hirugaruga@users.noreply.github.com>
2025-12-01 15:51:32 +0300

    Update test_1.cpp
    add hmwork12

--- test_cpp/test_1.cpp
+++ test_cpp/test_1.cpp
@@ -222,4 +222,201 @@ int main() {
 
 }
 
+#include <iostream>
+#include <optional>
+#include <random>
+#include <cmath>
+
+#include <stack>
+
+
+namespace problem_one {
+	//задание 1.1. Точный алгоритм для разности
+	double smth(int a, int b) {
+		if (a == b && a % 2 == 0) return 0;
+		if (a == b && a % 1 == 0) return 1;
+		return 0;
+
+	}
+	//задание 1.2. Приблеженный алгоритм для разности
+	// Методом кавальери или рандомом ;/
+
+	double diff_black_white(double a, double b) {
+		int n = 1e6;
+		std::random_device rd;
+		std::mt19937_64 gen(rd());
+		std::uniform_real_distribution<double> dist_x(0, a);
+		std::uniform_real_distribution<double> dist_y(0, b);
+		long long black = 0;
+		long long white = 0;
+
+		for (int i = 0; i < n; i++) {
+			double x = dist_x(gen);
+			double y = dist_y(gen);
+			if (y <= b * (1 - x / a)) {
+				int rx = (int)x;
+				int ry = (int)y;
+				if ((rx + ry) % 2 == 0) ++black;
+				else ++white;
+			}
+			else n--;
+		}
+		double s = a * b / 2;
+		double black_s = s * black / n * 2;
+		double white_s = s * white / n * 2;
+		return black_s - white_s;
+		}
+
+
+
+	}
+//задание 2.1. Дерево поиск  
+
+namespace problem_two {
+
+	template <typename TKey, typename TData>
+	class binary_search_tree
+	{
+		struct node {
+			TKey key; TData data; node* left, right; size_t size;
+			node(const TKey& k, const TData& d) : key(k), data(d), left(nullptr), right(nullptr) {};
+		}
+		*root = nullptr;
+		size_t count = 0;
+
+		size_t get_size(node* n) const { return n ? n->size : 0; }
+
+
+	public:
+
+
+		binary_search_tree() {};
+		
+		~binary_search_tree();
+
+		std::optional<TData> find(const TKey& key);
+
+		void insert(const TKey& key, const TData& data);
+
+		std::optional<TKey> findnext(const TKey& key);
+
+		void remove(const TKey& key);
+
+		//void remove_alt(const TKey& key);
+
+	};
+
+	template<typename TKey, typename TData>
+	std::optional<TData> binary_search_tree<TKey, TData>::find(const TKey& key)
+	{
+		node* current = root;
+		while (current != nullptr && current->key != key) {
+			current = current->key > key ? current->left: current->right;
+		}
+		return (current != nullptr && current->key == key) ? current->data : std::nullopt;
+	}
+
+	template<typename TKey, typename TData>
+	void binary_search_tree<TKey, TData>::insert(const TKey& key, const TData& data)
+	{
+		node* current = root;
+		node* parent = nullptr;
+		while (current != nullptr && current->key != key) {
+			parent = current;
+			current = current->key > key ? current->left : current->right;
+		}
+		if (current != nullptr && current->key == key) current->data = data;
+		else {
+			node* tmp = new node{ key, data };
+			if (parent == nullptr) root = tmp;
+			else if (key < parent->key) parent->left = tmp;
+			else parent->right = tmp;
+			count++;
+		}
+	}
+
+	template<typename TKey, typename TData>
+	std::optional<TKey> binary_search_tree<TKey, TData>::findnext(const TKey& key)
+	{
+		node* current = root;
+		node* last_left = nullptr;
+		while (current != nullptr && current->key != key) {
+			if (current->key > key) {
+				last_left = current;
+				current = current->left;
+			}
+			else current = current->right;
+		}
+		if (current->right != nullptr) {
+			current = current->right;
+			while (current->left != nullptr) current = current->left;
+			return current->key;
+		}
+		else return last_left != nullptr ? last_left->key: std::nullopt;
+	}
+
+	template<typename TKey, typename TData> 
+	void binary_search_tree<TKey, TData>::remove(const TKey& key)
+	{
+		node* current = root;
+		node* parent = nullptr;
+		while (current != nullptr && current->key != key) {
+			parent = current;
+			current = current->key > key ? current->left : current->right;
+		}
+		if (current == nullptr) return;
+		if (current->left == nullptr && current->right == nullptr) {
+			if (parent == nullptr) root = nullptr;
+			else if (parent->left == current) parent->left = nullptr;
+			else parent->right = nullptr;
+		} else if (current->left == nullptr || current->right == nullptr) {
+			node* child = current->left != nullptr ? current->left : current->right;
+			if (parent == nullptr) root = child;
+			else if (parent->left == current) parent->left = child;
+			else parent->right = child;
+		} else {
+			node* A = current->right;
+			node* A_parent = current;
+			while (A->left != nullptr) {
+				A_parent = A;
+				A = A->left;
+			}
+			current->key = A->key;
+			current->data = A->data;
+
+			if (A_parent->left == A) A_parent->left = A->right;
+			else A_parent->right = A->right;
+			delete A;
+			count--;
+			return;
+		}
+		delete current;
+		count--;
+
+	}
+
+	template<typename TKey, typename TData>
+	binary_search_tree<TKey, TData>::~binary_search_tree()
+	{
+		std::stack<node*> S;
+		node* current = root;
+		while (current != nullptr || !S.empty()) {
+			if (current != nullptr) {
+				S.push(current);
+				current = current->left;
+			} else {
+				current = S.top()->right;
+				delete S.top();
+				S.pop();
+			}
+		}
+	}
+}
+
+
+int main() {
+	std::cout << problem_one::diff_black_white(3, 3) << std::endl;
+
+}
+
 
